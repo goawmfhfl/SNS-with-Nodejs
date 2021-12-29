08df8ee5349285a7cb449f6a04d452c8be590cf7
jae <goawmfhfl1@naver.com>
2021-12-29 22:07:39 +0900

    Add: DataFormat
    1. 데이터 포멧 직접 만듦
    2. 만들고자하는 데이터 포멧 형태는 2000-12-29

--- pet-sns/controller/postCtr.js
+++ pet-sns/controller/postCtr.js
@@ -1,11 +1,28 @@
 const Post = require("../model/post");
 
+/* 우리가 원하는 데이터 포멧을 직접 만들기 */
+// 2020-12-29 형태로 나오게 해야함
+const formatDate = date => {
+  let d = new Date(date);
+  let month = "" + (d.getMonth() + 1);
+  let day = "" + d.getDate();
+  let year = d.getFullYear();
+  if (month.length < 2) {
+    month = `0 ${month}`;
+  }
+  if (day.length < 2) {
+    day = `0 ${day}`;
+  }
+  return [year, month, day].join("-");
+};
+
 const postCtr = {
   upload: async (req, res) => {
     const { title, content } = req.body;
     // 이미지를 불러와야한다. s3에 업로드를 활용하는 방법을 알아보자
     const image = req.file;
-    console.log(image);
+    const publishedDate = formatDate(new Date());
+    // 2020-12-29 형태로 나오게 해야함
   },
 };
 
