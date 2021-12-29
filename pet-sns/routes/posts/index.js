459996c796c1756535d7c11209ffe5dd2886e560
jae <goawmfhfl1@naver.com>
2021-12-29 21:56:34 +0900

    Add: postCtr
    1. post 컨트롤러 모듈 추가
    2. 외부 모듈을 require을 통해서 사용한다

--- pet-sns/routes/posts/index.js
+++ pet-sns/routes/posts/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const upload = require("../../module/multer");
+const postCtr = require("../../controller/postCtr");
 
 router.get("/upload", (req, res) => {
   res.render("upload");
@@ -16,6 +17,6 @@ router.get("/update/:id", (req, res) => {
 // 내부에서 로직을 작성하는 방식은 유지보수가 힘들다
 // 그렇기 때문에 controller라는 폴더를 생성해서
 // post에 대한 로직을 외부에서 작성해서 사용한다.
-router.post("/", upload.single("image"), (req, res) => {});
+router.post("/", upload.single("image"), postCtr.upload);
 
 module.exports = router;
