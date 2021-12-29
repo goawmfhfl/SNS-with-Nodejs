25bf4b5f8134ccea2c3646cd637308cb2b7a93f9
jae <goawmfhfl1@naver.com>
2021-12-29 11:07:49 +0900

    Add: render index
    1. index 파일 렌더
    2. / 링크로 접속할 경우 get 요청으로 인하여 index.ejs파일을 렌더해서 보여준다

--- pet-sns/routes/index.js
+++ pet-sns/routes/index.js
@@ -1,9 +1,12 @@
-var express = require('express');
+var express = require("express");
+const res = require("express/lib/response");
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", (req, res) => {
+  res.render("index", { postList: [] });
 });
+
+// "/" 링크로 접속했을 때 view폴더에 있는 index파일을 먼저 보여준다
+// 게시물들에 대한 정보를 postList에 담아온다
 
 module.exports = router;
