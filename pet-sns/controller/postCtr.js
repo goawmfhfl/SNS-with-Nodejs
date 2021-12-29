5e8fe9a23ae7a6327148213e3b1c6f643600c720
jae <goawmfhfl1@naver.com>
2021-12-30 07:31:44 +0900

    Add: Post 구현
    1. Post 요청시 DataBase에 post변수에 지정된 형태로 저장
    2. 사용자가 post 요청을 했을 때의 정보를 가공해서 DB에 저장한다. 그리고 저장된 정보를 다시 DB에서 받아온 다음에 가공해서 데이터 바인딩을 하면 된다

--- pet-sns/controller/postCtr.js
+++ pet-sns/controller/postCtr.js
@@ -20,9 +20,21 @@ const postCtr = {
   upload: async (req, res) => {
     const { title, content } = req.body;
     // 이미지를 불러와야한다. s3에 업로드를 활용하는 방법을 알아보자
-    const image = req.file;
-    const publishedDate = formatDate(new Date());
-    // 2020-12-29 형태로 나오게 해야함
+    const image = req.file.location;
+    const publisheDate = formatDate(new Date());
+    const post = new Post({
+      title: title,
+      content: content,
+      image: image,
+      publisheDate: publisheDate,
+    });
+    try {
+      await post.save();
+      // 저장하고 다시 메인페이지로 이동
+      res.redirect("/");
+    } catch (error) {
+      res.status(500).send("upload error");
+    }
   },
 };
 
