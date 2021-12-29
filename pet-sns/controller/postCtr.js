43536356dba7218edf44386c97e597cf4826bca5
jae <goawmfhfl1@naver.com>
2021-12-29 21:03:05 +0900

    Fix: 디버그
    1. 오탈자 수정

--- pet-sns/controller/postCtr.js
+++ pet-sns/controller/postCtr.js
@@ -4,7 +4,9 @@ const postCtr = {
   upload: async (req, res) => {
     const { title, content } = req.body;
     // 이미지를 불러와야한다. s3에 업로드를 활용하는 방법을 알아보자
-    const imgae = req.file;
+    const image = req.file;
     console.log(image);
   },
 };
+
+module.exports = postCtr;
