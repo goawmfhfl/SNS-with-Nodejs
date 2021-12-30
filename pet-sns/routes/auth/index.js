792849c4718a0dcd369b64b83e764c07e78d89e8
jae <goawmfhfl1@naver.com>
2021-12-30 13:25:07 +0900

    Del: 필요없는 부분 제거

--- pet-sns/routes/auth/index.js
+++ pet-sns/routes/auth/index.js
@@ -13,12 +13,5 @@ router.get("/login", (req, res) => {
   res.render("login");
 });
 
-// 회원가입 라우터 구현
-// 로그인 url 접속 시에 register ejs 파일을 보여준다
-// auth/login
-router.get("/register", (req, res) => {
-  res.render("register");
-});
-
 // 라우터를 외부 파일에서도 사용할 수 있게 세팅해주었다
 module.exports = router;
