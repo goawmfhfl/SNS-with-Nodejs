c2a69c0fbb793c1fc1985dfb92d762b65e75c145
jae <goawmfhfl1@naver.com>
2021-12-30 20:27:47 +0900

    Add: middleware 설정
    1. 미들웨어를 설정했다.
    2. 사용자가 어떤 요청을 할 때마다 인증과정을 거쳐야한다 그렇기 떄문에  app.use("/", indexRouter); 문이 실행되기 이전에
    app.use(jwtMiddleware);를 통해서 middleware를 실행시킨다.
    
    물론 미들웨어 함수를 사용하기 위해서는 const jwtMiddleware = require("./module/jwtMiddleware");로
    불러와줘야한다.

--- pet-sns/app.js
+++ pet-sns/app.js
@@ -3,6 +3,7 @@ var express = require("express");
 var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
+const jwtMiddleware = require("./module/jwtMiddleware");
 
 var indexRouter = require("./routes/index");
 
@@ -18,6 +19,10 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
+//라우터를 모든 라우터에 접근할 때 사용자의 정보를 파악해줘야함
+//라우터 앞단에 middleware를 넣는다.
+// 사용자가 로그인되어있는지 안되어있는지 미들웨어를 통해서 요청 결과값을 받을 수 있다
+app.use(jwtMiddleware);
 app.use("/", indexRouter);
 
 // catch 404 and forward to error handler
