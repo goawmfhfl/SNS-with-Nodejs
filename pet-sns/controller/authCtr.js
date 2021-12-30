3cc97bc25096775f3cd4341ebc27a5fb4b2d8613
jae <goawmfhfl1@naver.com>
2021-12-30 18:32:41 +0900

    Add: 로그인 authCtr 구현
    1. 로그인 시 get요청에 의해 보여지는 page가 아닌 사용자의 정보를 보내기 때문에 post 요청을 보내게 된다
    
    2. post에는 유저의 아이디와 비밀번호의 정보가 함께 전송이 된다.
    전송된 정보는 req.body에 담겨오게 된다.
    username,password를 구조분해할당한 후에 요리를 해줄 것이다.
    변수 exist에서 database에 username이 있는지 확인한다.
    확인해서 존재할 경우에는 User클래스에 담기게 된다.
    
    user 클래스에 password는 그냥 담지 않을 것이다. bcrypt 패키지를 통해서 hashdpassword로 만들어준다.
    hashedpassword된 정보를 유저클래스에 넣어준다.
    
    위는 회원가입에 대한 로직이였다.
    
    아래는 login 요청이 들어오게 될 경우의 예시이다. login을 할 경우 역시나 req.body를 통해서 정보를 전달 받는다
    
    DB에서 유저가 있는지 검색을 한다
    만약 일치한다면 현재 입력한 비밀번호와 토크나이징 된 비밀번호와 비교한다 이때 비교할 때는 bcrypt.compare를 꼭 사용해야한다.
    만약 정보가 일치한다면 ? userData를 JSON으로 바꿔준 후 data 변수에 담아준다. 그리고 유저의 패스워드를 지워버린다.
    그런후의 유저의 고유 아이디값과 아이디값을 토크나이징, 시크릿 키 설정, 그리고 유효기간을 정한다.
    
    그리고 사용자에게 응답값으로 cookie에 token을 넣어서 보내고 유효기간을 정확히 명시하며 http 요청에 의해서만 전달이 되도록 한다.

--- pet-sns/controller/authCtr.js
+++ pet-sns/controller/authCtr.js
@@ -1,36 +1,78 @@
 const User = require("../model/auth");
 // 문자열을 암호화하는데 빠르게 도와준다.
 const bcrypt = require("bcrypt");
+const secretKey = require("../config/secretKey.json");
+// jwt 패키지 불러오기
+const jwt = require("jsonwebtoken");
 
 // 레지스터 관련 코드 작성하는 곳
 const authCtr = {
   register: async (req, res) => {
-    // 사용자의 요청으로 부터 username과 password를 req.body에서 꺼내온다
     const { username, password } = req.body;
-    // 유저네임과 일치하는지 확인을 해야한다
-    // 사용자 정보를 확인해야한다.
-    // 일치하는 항목이 존재하면
     const exist = await User.findOne({ username: username });
-    // findOne을 통해 트루시인지 폴시값인지 알 수 있다.
     if (exist) {
       res.status(504).send("user exist");
       return;
     }
-
-    // 일치하지 않다면 회원가입을 진행해야된다
     const user = new User({
       username: username,
     });
-
-    // password를 그대로 데이터베이스에 그대로 저장하면 위험하다
-    // 그렇기에 암호화를 통해서 저장해야한다
     const hashedPassword = await bcrypt.hash(password, 10);
-    // 비크립트 알고리즘을 사용해서 password를 해쉬 알고리즘을 10번이나 돌린다
     user.password = hashedPassword;
     await user.save();
     res.redirect("/");
+
+    login: async (req, res) => {
+      const { username, password } = req.body;
+      // 사용자의 요청에 담긴 정보(req.body)를 통해서
+      // 그 정보를 토대로 DB를 검색해서 찾아본다.
+      const user = await User.findOne({ username: username });
+      // 만약 유저가 아니라면 user not found를 출력
+      if (!user) {
+        res.status(500).send("user not found!");
+        return;
+      }
+      // 만약  유저가 맞다면
+      // 유저의 패스워드 데이터 베이스 패스워드 확인해야함
+      // user.password(데이터 베이스에 있는 비밀번호)
+      //요청받은 password (현재 클라이언트가 요청하는 비밀번호)
+      const valid = await bcrypt.compare(password, user.password);
+      // 만약 일치하지 않으면 아래와 같은 if문이 작동한다.
+      if (!valid) {
+        res.status(500).send("password invalid");
+      }
+      // 만약 일치한다면?
+      const data = user.toJSON;
+      // 패스워드를 왜 지워주는걸까?
+      delete data.password;
+      // 토크나이징
+      const token = jwt.sign(
+        {
+          _id: data._id,
+          username: username,
+        },
+        secretKey.key,
+        // 외부 파일 config에서 key값을 불러왔다.
+        // 해당 토근은 기한을 두는 것이다
+        // 실무에서는 30분, 1시간 등등 짧게 한다
+        // 하지만 나는 연습이기에 일주일로정해주었다.
+        {
+          expriresIn: "7d",
+        },
+      );
+      // 두번째 인자는 이를 암호화 할 수 있는 시크릿 키를 입력해야한다.
+      // 문자열 형태로 입력을 해야하는데 암호화 복호화는 보안이 중요하기 떄문에
+      // 외부파일로 옮겨준다
+      res.cookie("access_token", token, {
+        maxAge: 1000 * 60 * 60 * 24 * 7,
+        httpOnly: true,
+      });
+      // maxAge의 값은 milliseconds 단위를 사용하기 떄문에 1000 millisecondes
+      // 1(초) * 60(초) * 60(분) * 24(시간) * 7(일) = 7일
+      // http에서만 접근이 가능하게 설정
+      res.redirect("/");
+    };
   },
 };
 
-// 외부파일 사요하게 만들기
 module.exports = authCtr;
--- pet-sns/routes/auth/index.js
+++ pet-sns/routes/auth/index.js
@@ -5,6 +5,7 @@ const authCtr = require("../../controller/authCtr");
 // 회원가입 기능 구현하기
 // register라는 경로에 접속하면 회원가입이 진행되도록하자
 router.post("/register", authCtr.register);
+router.post("/login");
 
 // 로그인 라우터 구현
 // 로그인 url 접속 시에 login ejs 파일을 보여준다
