5da5c5a5064d5d31b449dc7f8ed53a8802c78c35
jae <goawmfhfl1@naver.com>
2021-12-31 22:14:42 +0900

    Add: rating component
    1. Rating 컴포넌트 구현
    2. Rating을 구현하기 위해서 Rating 컴포넌트를 따로 만들어 주었다
    
    Rating 컴포넌트에는 Props값으로 product의 rating 정보와 numReviews 정보를 전달한다.
    
    Rating 컴포넌트 생성이 되게 신박하고 재미있었는데 나는 평소에 별점을 어떻게 구현해야 할까 많은 고민을 했었다. 근데 i태그에 삼항연산자를 쓴 것을 보고 !! 무릎을 탁 치게 되었다.
    
    만약 전달받은 별 점 리뷰 점수는 거의 혁명급이다 너무 재밌다. 점수에 맞게 별점을 채울 수 있는 구조다.
    
    color값을 고정적으로 주기 위해서 디폴트 값으로 props값을 준것도 신기하다 새로운 것을 배웠다.
    
    Col 태그에는 key값을 넣어주었는데 이것에 대해서는 다음번에 자세히 이해 할 수 있을 것 같다

--- practice-sns/udemy/src/components/Product.js
+++ practice-sns/udemy/src/components/Product.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Card } from "react-bootstrap";
+import Rating from "./Rating";
 // Product의 부모 컴포넌트 HomeScreen을 통해서
 // product props를 전달받는다.
 const Product = ({ product }) => {
@@ -17,12 +18,13 @@ const Product = ({ product }) => {
           </a>
 
           <Card.Text as="div">
-            <div className="my-3">
-              {product.rating} from {product.numReviews} reviews
-            </div>
+            <Rating
+              value={product.rating}
+              text={`${product.numReviews} reviews`}
+            ></Rating>
           </Card.Text>
 
-          <Card.Text as="h3">${product.sign}</Card.Text>
+          <Card.Text as="h3">${product.price}</Card.Text>
         </Card.Body>
       </Card>
     </div>
---
+++ practice-sns/udemy/src/components/Rating.js
@@ -0,0 +1,77 @@
+import React from "react";
+import propTypes from "prop-types";
+
+// propTypes 패키지를 사용하면 확장을 편하게 사용할 수 있다.
+
+// 색상을 전달받는다고..???
+const Rating = ({ value, text, color }) => {
+  return (
+    <div className="rating">
+      <span>
+        <i
+          style={{ color }}
+          className={
+            value >= 1
+              ? "fas fa-star"
+              : value >= 0.5
+              ? "fas fa-star-half-alt"
+              : "far fa-star"
+          }
+        ></i>
+        <i
+          style={{ color }}
+          className={
+            value >= 2
+              ? "fas fa-star"
+              : value >= 1.5
+              ? "fas fa-star-half-alt"
+              : "far fa-star"
+          }
+        ></i>
+        <i
+          style={{ color }}
+          className={
+            value >= 3
+              ? "fas fa-star"
+              : value >= 2.5
+              ? "fas fa-star-half-alt"
+              : "far fa-star"
+          }
+        ></i>
+        <i
+          style={{ color }}
+          className={
+            value >= 4
+              ? "fas fa-star"
+              : value >= 3.5
+              ? "fas fa-star-half-alt"
+              : "far fa-star"
+          }
+        ></i>
+        <i
+          style={{ color }}
+          className={
+            value >= 5
+              ? "fas fa-star"
+              : value >= 4.5
+              ? "fas fa-star-half-alt"
+              : "far fa-star"
+          }
+        ></i>
+      </span>
+      <span>{text && text}</span>
+    </div>
+  );
+};
+
+Rating.defaultProps = {
+  color: "#FE7E6D",
+};
+
+Rating.propTypes = {
+  value: propTypes.number.isRequired,
+  text: propTypes.string.isRequired,
+  color: propTypes.string,
+};
+
+export default Rating;
