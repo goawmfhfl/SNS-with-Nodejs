2b5c1bc3d7aace30e77e08e64ae35949c99fd5c1
jae <goawmfhfl1@naver.com>
2022-01-05 16:23:44 +0900

    Add: Cart Screen
    역시나 삼항연산자를 통해서 만약 cartItmes.lenght 값이 0이라면
    현재 아무것도 없다는 것을 의미하니깐 Message 컴포넌트만 보여준다.
    cartItems을 map 함수로 감싼후에 하나하나의 아이템들을 item으로 메핑한다
    참고로 현재 cartItems를 조회하면 즉 카트에 담긴 아이템들의 값이 나오게 된다.
    cartItems: (4) [{…}, {…}, {…}, {…}]
    4개를 담았으니 4개의 값이 매핑되어 나올 것이다
    
    Form.Control을 통해서 Form의 값에는 addTOCart를 통하여 액션값을 넣어준다
    액션값을 넣어주게된다면 전역 변수의 값은 CartReducer에 의하여 옵션에 맞게 설정이 되며
    최종적으로 store의 reducercombine에 할당이 된다.
    
    또한 두번째 요소로 QtyValue를 전달함으로써 클릭한 값이 전역변수의 값이 되도록 설정한다
    클릭을 해야할 요소가 필요하지 않겠는가? 요소를 클릭하기 위해서는 Option을 사용하도록하고
    Option의 요소들은 countInStock요소의 빈 객체를 만들어주고 키값을 조회하는 방식으로 만든다
    해당 키값에는 0번째부터의 인덱스값이 저장이된다. 갯수에맞게 option에 key값과 value값 지정
    
    나머지 전체 총합의 값을 구할때는 cartItems에서 값을 조회해오고 그런후에 reduce를 써서
    총 합을 구해준다. 여기가 제일 재밌는부분이기도하다 나중에 프로젝트에 써먹어야겠다.

--- practice-sns/udemy/FrontEnd/src/screens/CartScreen.js
+++ practice-sns/udemy/FrontEnd/src/screens/CartScreen.js
@@ -1,6 +1,11 @@
 import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { useParams, useSearchParams } from "react-router-dom";
+import {
+  useParams,
+  useSearchParams,
+  Link,
+  useNavigate,
+} from "react-router-dom";
 import {
   Row,
   Col,
@@ -8,7 +13,7 @@ import {
   Image,
   Form,
   Button,
-  card,
+  Card,
 } from "react-bootstrap";
 import Message from "../components/Message";
 import { addToCart } from "../actions/CartAction";
@@ -16,12 +21,13 @@ import { addToCart } from "../actions/CartAction";
 const CartScreen = () => {
   const { id } = useParams();
   const [searchParams] = useSearchParams();
+  const navigate = useNavigate();
   const qty = Number(searchParams.get("qty"));
   const dispatch = useDispatch();
 
   const cart = useSelector(state => state.cart);
   const { cartItems } = cart;
-  console.log(cartItems);
+  console.log(cartItems[0].qty);
   // useSelector는 전체 전역에 있는 reducer들을 담고있다.
   // state  // productList: {…}, productDetails: {…}, cart: {…}}
   // state.cart  // cartItems: (4) [{…}, {…}, {…}, {…}]
@@ -32,7 +38,93 @@ const CartScreen = () => {
     }
   }, [dispatch, id, qty]);
 
-  return <div>CartScreen</div>;
+  const removeFromCartHandler = () => {
+    console.log("remove");
+  };
+  const checkoutHandler = () => {
+    navigate("/login/?redirect=shipping");
+  };
+
+  return (
+    <Row>
+      <Col md={8}>
+        <h1>Shopping Cart</h1>
+        {cartItems.length === 0 ? (
+          <Message>
+            Yoyr cart is empty<Link to="/">go Back</Link>
+          </Message>
+        ) : (
+          <ListGroup variant="flush">
+            {cartItems.map(item => (
+              <ListGroup.Item>
+                <Row>
+                  <Col md={2}>
+                    <Image src={item.image} alt={item.name} fluid rounded />
+                  </Col>
+                  <Col md={3}>
+                    <Link to={`/product/${item.porduct}`}>{item.name}</Link>
+                  </Col>
+                  <Col md={2}>{item.price}</Col>
+                  <Col md={2}>
+                    {" "}
+                    <Form.Control
+                      as="select"
+                      value={item.qty}
+                      onChange={e =>
+                        dispatch(
+                          addToCart(item.product, Number(e.target.value)),
+                        )
+                      }
+                    >
+                      {[...Array(item.countInStock).keys()].map(x => (
+                        <option key={x + 1} value={x + 1}>
+                          {x + 1}
+                        </option>
+                      ))}
+                    </Form.Control>
+                  </Col>
+                  <Col md={2}>
+                    <Button
+                      type="button"
+                      variant="light"
+                      onClick={() => removeFromCartHandler(item.product)}
+                    >
+                      <i className="fas fa-trash"></i>
+                    </Button>
+                  </Col>
+                </Row>
+              </ListGroup.Item>
+            ))}
+          </ListGroup>
+        )}
+      </Col>
+      <Col md={4}>
+        <Card>
+          <ListGroup>
+            <ListGroup className="item">
+              <h2>
+                Subtotal({cartItems.reduce((acc, item) => acc + item.qty, 0)})
+                item
+              </h2>
+              {cartItems
+                .reduce((acc, item) => acc + item.qty * item.price, 0)
+                .toFixed(2)}
+            </ListGroup>
+          </ListGroup>
+          <ListGroup>
+            <Button
+              type="button"
+              className="btn-block"
+              disabled={cartItems.lenrght === 0}
+              onClick={checkoutHandler}
+            >
+              Proceed To Checkout
+            </Button>
+          </ListGroup>
+        </Card>
+      </Col>
+    </Row>
+  );
 };
 
 // split을 사용하여
